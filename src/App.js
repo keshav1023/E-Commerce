c3fffb9fff10899fc4ffba350d3cdbfffe23ed31
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-12-12 17:48:10 +0530

    signup login page added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-      <Home>
-        
-      </Home>
+    <SignupPage></SignupPage>
     </div>
   );
 }

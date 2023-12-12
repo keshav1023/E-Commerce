57761d539863714d011637aa95e240cbb4fcb105
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-12-12 17:57:09 +0530

    signup login page routing done

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -5,6 +5,7 @@ import {
   incrementAsync,
   selectCount,
 } from '../../counter/counterSlice';
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
   const count = useSelector(selectCount);
@@ -99,9 +100,9 @@ export default function Signup() {
 
         <p className="mt-10 text-center text-sm text-gray-500">
           Already a Member?{' '}
-          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+          <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
             Log In
-          </a>
+          </Link>
         </p>
       </div>
     </div>
